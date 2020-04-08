ca5925b5e8e09c48fa6128cd5c959d124791a0e9
zain <zainabedeen.sohail@gmail.com>
2020-04-08 17:46:26 +0500

    Updated.

--- src/invitation/invitation.service.ts
+++ src/invitation/invitation.service.ts
@@ -41,74 +41,6 @@ export class InvitationService {
   ) {
   }
 
-  // async invited(companyId: string, emails: string, user: UserInterface): Promise<AddInvitationResponseInterface[]> {
-  //   const company: CompanyInterface = await this.companyService.getIfCompanyExistsAndIAmAdmin(companyId, user._id);
-  //   const existingUsers: UserInterface[] = await this.userService.findAllWhere({
-  //     email: {
-  //       $in: emails,
-  //     },
-  //     isActive: true,
-  //     companies: {
-  //       $elemMatch: {
-  //         company: companyId,
-  //         isActive: true,
-  //       },
-  //     },
-  //   });
-  //   const existingInvitationsOfCompany: InvitationInterface[] = await this.findAllWhere({
-  //     company: companyId,
-  //     isActive: true,
-  //     isAccepted: false,
-  //   });
-  //   const newInvitations: InvitationInterface[] = [];
-  //   const response: AddInvitationResponseInterface[] = [];
-  //   for (const email of emails) {
-  //     const userExisted = existingUsers.find(user => user.email === email);
-  //     if (userExisted) {
-  //       response.push({
-  //         invitationId: null,
-  //         message: ErrorMessages.ALREADY_MEMBER.replace('?', email),
-  //         email,
-  //       });
-  //       continue;
-  //     }
-  //     const existingInvitation: InvitationInterface = existingInvitationsOfCompany?.find(invite => invite.email === email);
-  //     if (existingInvitation) {
-  //       response.push({
-  //         invitationId: null,
-  //         message: ErrorMessages.ALREADY_INVITED.replace('?', email),
-  //         email,
-  //       });
-  //       continue;
-  //     }
-  //     if (company.subscription.maxUsers && company?.users?.length + existingInvitationsOfCompany?.length + newInvitations.length >= company.subscription.maxUsers) {
-  //       response.push({
-  //         invitationId: null,
-  //         message: ErrorMessages.MAX_USER_LIMIT_REACHED,
-  //         email,
-  //       });
-  //       continue;
-  //     }
-  //     newInvitations.push({
-  //       email,
-  //       invitedBy: user._id,
-  //       company: companyId,
-  //     });
-  //   }
-  //   if (newInvitations.length) {
-  //     const insertedInvitations: InsertedInvitationsInterface[] = await this.insertMany(newInvitations);
-  //     for (const invitation of insertedInvitations) {
-  //       response.push({
-  //         invitationId: invitation._doc._id,
-  //         email: invitation._doc.email,
-  //         message: ResponseMessages.INVITED_SUCCESSFULLY.replace('?', invitation._doc.email),
-  //       });
-  //       this.sendInvitationLink(invitation._doc._id, user, invitation._doc, company);
-  //     }
-  //   }
-  //   return response;
-  // }
-
 
   async invite(companyId: string, email: string, owner: UserInterface): Promise<AddInvitationResponseInterface> {
 
@@ -123,9 +55,17 @@ export class InvitationService {
         email,
       }
     }
+    const checkIfUserAlreadyInvited = await this.userService.findOneWhere({ email })
 
-    const user = await this.userService.findOneWhere({ email })
-    if (!user) {
+
+    if (checkIfUserAlreadyInvited) {
+      response = {
+        message: ErrorMessages.GENERIC_CONFLICT,
+        email,
+      };
+      return response
+    }
+
       const newUser = await this.authService.registerBroker({ email, password: (await this.stringHelper.generateRandomString(8)).toString() }, companyId)
 
       try {
@@ -158,65 +98,6 @@ export class InvitationService {
       }
     }
 
-    else {
-      const checkIfUserAlreadyInvited = await this.userService.findAllWhere({
-        _id: {
-          $ne: user._id,
-        },
-        isActive: true,
-        companies: {
-          $elemMatch: {
-            company: companyId,
-            isActive: true,
-          },
-        },
-      });
 
-      if (checkIfUserAlreadyInvited) {
-        response = {
-          message: ErrorMessages.ALREADY_INVITED.replace('?', email),
-          email,
-        };
-        return response
-      }
-
-
-      try {
-        session = await this.userModel.db.startSession();
-        await session.startTransaction();
-        await this.userService.findOneAndUpdateWhere({
-          isActive: true,
-          isEmailVerified: true,
-          $push: {
-            companies: {
-              company: companyId,
-            },
-          },
-        }, {
-          _id: user._id,
-        }, { session, lean: true });
-        await this.companyService.findOneAndUpdateWhere({
-          $push: {
-            users: {
-              user: user._id,
-            },
-          },
-        }, {
-          _id: companyId,
-        }, { session, lean: true });
-        await session.commitTransaction();
-        response = {
-          email: email,
-          message: ResponseMessages.INVITED_SUCCESSFULLY.replace('?', email),
-        };
-        return response;
-
-      } catch (e) {
-        console.warn(e);
-        session.abortTransaction();
-        throw new InternalServerErrorException(defaultInternalServerErrorResponse);
-      }
-
-    }
-  }
+  
 }
